99d721c06d94fab5d32d49dd8ef1d9a800567fe5
Madhushree <75978667+MMadhushree@users.noreply.github.com>
2023-09-05 17:27:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,124 @@
 import speech_recognition as sr
-import pyttsx3
-import pywhatkit
+import os
+import webbrowser
+import openai
+from config import apikey
 import datetime
-import wikipedia
-import pyjokes
+import random
+import numpy as np
 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+chatStr = ""
 
 
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
+def chat(query):
+    global chatStr
+    print(chatStr)
+    openai.api_key = apikey
+    chatStr += f"User: {query}\n Jarvis: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
 
 
-def take_command():
+def ai(prompt):
     try:
+        openai.api_key = apikey
+        text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        text += response["choices"][0]["text"]
+        if not os.path.exists("Openai"):
+            os.mkdir("Openai")
+
+        with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip()}.txt", "w") as f:
+            f.write(text)
+    except Exception as e:
+        print(f"An error occurred in the 'ai' function: {str(e)}")
+
+
+def say(text):
+    try:
+        os.system(f'say "{text}"')
+    except Exception as e:
+        print(f"An error occurred in the 'say' function: {str(e)}")
+
+
+def takeCommand():
+    try:
+        r = sr.Recognizer()
         with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
-    return command
+            audio = r.listen(source)
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        return ""
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {str(e)}")
+        return "Some Error Occurred. Sorry from Jarvis"
+    except Exception as e:
+        print(f"An error occurred in the 'takeCommand' function: {str(e)}")
+        return "Some Error Occurred. Sorry from Jarvis"
 
 
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who is' in command:
-        person = command.replace('who is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
+if __name__ == '__main__':
+    print('Welcome to Jarvis A.I')
+    say("Jarvis A.I")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"],
+                 ["google", "https://www.google.com"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
 
+        if "open music" in query:
+            musicPath = "/Users/harry/Downloads/downfall-21371.mp3"
+            os.system(f"open {musicPath}")
 
-while True:
-    run_alexa()
+        elif "the time" in query:
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} bajke {min} minutes")
+
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
+
+        elif "open pass".lower() in query.lower():
+            os.system(f"open /Applications/Passky.app")
+
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
+
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
